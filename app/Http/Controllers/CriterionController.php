b2832eda4d9969948fd30e096a50a13a9c2b85d9
eduardo <es9828478@gmail.com>
2025-12-04 18:29:40 -0600

    Actualizacion nueva2

--- app/Http/Controllers/CriterionController.php
+++ app/Http/Controllers/CriterionController.php
@@ -26,7 +26,7 @@ class CriterionController extends Controller
     {
         return view('criteria.create');
     }
-     //Se agrega un comentario
+     //Se agrega un comentario ffdfjfjfj
     /**
      * Store a newly created resource in storage.
      */
