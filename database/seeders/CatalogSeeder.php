64750c0fcaa2a427a09a99452c5067ace4246a71
eduardo <es9828478@gmail.com>
2025-12-09 10:00:19 -0600

    Se agregaron mas carreras al catalogSeeder

--- database/seeders/CatalogSeeder.php
+++ database/seeders/CatalogSeeder.php
@@ -23,16 +23,39 @@ class CatalogSeeder extends Seeder
             Criterion::firstOrCreate($crit);
         }
 
-        // 2. Carreras
+        // 2. Carreras del TecNM
         $carreras = [
+            // Ingenierías
             ['name' => 'Ingeniería en Sistemas Computacionales', 'code' => 'ISC'],
             ['name' => 'Ingeniería en Gestión Empresarial', 'code' => 'IGE'],
             ['name' => 'Ingeniería Industrial', 'code' => 'IND'],
             ['name' => 'Ingeniería Electrónica', 'code' => 'IET'],
-            // ... (agrega el resto de tus carreras aquí)
+            ['name' => 'Ingeniería Civil', 'code' => 'IC'],
+            ['name' => 'Ingeniería Mecánica', 'code' => 'IM'],
+            ['name' => 'Ingeniería Eléctrica', 'code' => 'IE'],
+            ['name' => 'Ingeniería Mecatrónica', 'code' => 'IMT'],
+            ['name' => 'Ingeniería Química', 'code' => 'IQ'],
+            ['name' => 'Ingeniería Bioquímica', 'code' => 'IBQ'],
+            ['name' => 'Ingeniería en Tecnologías de la Información y Comunicaciones', 'code' => 'ITIC'],
+            ['name' => 'Ingeniería en Logística', 'code' => 'ILOG'],
+            ['name' => 'Ingeniería Ambiental', 'code' => 'IA'],
+            ['name' => 'Ingeniería en Energías Renovables', 'code' => 'IER'],
+            ['name' => 'Ingeniería en Materiales', 'code' => 'IMAT'],
+            ['name' => 'Ingeniería en Nanotecnología', 'code' => 'INANO'],
+            ['name' => 'Ingeniería en Aeronáutica', 'code' => 'IAERO'],
+            ['name' => 'Ingeniería en Agronomía', 'code' => 'IAGRO'],
+            ['name' => 'Ingeniería en Industrias Alimentarias', 'code' => 'IIA'],
+            ['name' => 'Ingeniería Forestal', 'code' => 'IF'],
+            // Licenciaturas
+            ['name' => 'Licenciatura en Administración', 'code' => 'LA'],
+            ['name' => 'Licenciatura en Contaduría', 'code' => 'LC'],
+            ['name' => 'Licenciatura en Turismo', 'code' => 'LT'],
+            ['name' => 'Licenciatura en Biología', 'code' => 'LB'],
+            // Arquitectura
+            ['name' => 'Arquitectura', 'code' => 'ARQ'],
         ];
         foreach ($carreras as $carrera) {
-            Career::firstOrCreate($carrera);
+            Career::firstOrCreate(['code' => $carrera['code']], $carrera);
         }
 
         // 3. Especialidades
@@ -40,7 +63,7 @@ class CatalogSeeder extends Seeder
             ['name' => 'Desarrollo de Software / Web'],
             ['name' => 'Ciberseguridad y Redes'],
             ['name' => 'Inteligencia Artificial y Ciencia de Datos'],
-            // ... (resto de especialidades)
+            ['name' => 'Gestión de Proyectos de Software'],
         ];
         foreach ($especialidades as $sp) {
             Specialty::firstOrCreate($sp);
